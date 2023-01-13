204f83268b78e85abf046377ebe6e3730ae2bcb8
seolmin <seolmin@megazone.com>
2023-01-13 11:31:14 +0900

    refactor: change interface from static letter to object (#1)
    Signed-off-by: seolmin@megazone.com <seolmin@mz.co.kr>

--- src/cloudforet/cost_analysis/interface/grpc/plugin/data_source.py
+++ src/cloudforet/cost_analysis/interface/grpc/plugin/data_source.py
@@ -1,5 +1,8 @@
 from spaceone.api.cost_analysis.plugin import data_source_pb2, data_source_pb2_grpc
 from spaceone.core.pygrpc import BaseAPI
+from cloudforet.cost_analysis.service.data_source_service import DataSourceService
+from cloudforet.cost_analysis.info.data_source_info import PluginInfo
+from cloudforet.cost_analysis.info.common_info import EmptyInfo
 
 
 class DataSource(BaseAPI, data_source_pb2_grpc.DataSourceServicer):
@@ -9,12 +12,12 @@ class DataSource(BaseAPI, data_source_pb2_grpc.DataSourceServicer):
     def init(self, request, context):
         params, metadata = self.parse_request(request, context)
 
-        with self.locator.get_service('DataSourceService', metadata) as data_source_service:
-            return self.locator.get_info('PluginInfo', data_source_service.init(params))
+        with self.locator.get_service(DataSourceService, metadata) as data_source_service:
+            return self.locator.get_info(PluginInfo, data_source_service.init(params))
 
     def verify(self, request, context):
         params, metadata = self.parse_request(request, context)
 
-        with self.locator.get_service('DataSourceService', metadata) as data_source_service:
+        with self.locator.get_service(DataSourceService, metadata) as data_source_service:
             data_source_service.verify(params)
-            return self.locator.get_info('EmptyInfo')
+            return self.locator.get_info(EmptyInfo)
